eb2741c0475501968b408ff1b2e34bd318d0f181
yuto <namawasabi1229@gmail.com>
2020-08-11 10:43:01 +0900

    specify dirctory

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 module.exports = {
   onPostBuild: async ({ utils, inputs, constants }) => {
     try {
-      await utils.run(
-        'html-validate',
-        [
-          ...inputs.config ? ['--config', inputs.config] : [],
-          '--ext', inputs.ext,
-          constants.PUBLISH_DIR
-        ]
-      );
+      await utils.run("html-validate", [
+        ...(inputs.config ? ["--config", inputs.config] : []),
+        "--ext",
+        inputs.ext,
+        ...(inputs.dir
+          ? inputs.dir.split(",").map((dir) => {
+              return `${constants.PUBLISH_DIR}/${dir}`;
+            })
+          : constants.PUBLISH_DIR),
+      ]);
     } catch (error) {
       if (error.exitCode === 1) {
-        return utils.build.failBuild('Invalid HTML', { error });
+        return utils.build.failBuild("Invalid HTML", { error });
       }
-      return utils.build.failBuild('Unknown error', { error });
+      return utils.build.failBuild("Unknown error", { error });
     }
-  }
+  },
 };
